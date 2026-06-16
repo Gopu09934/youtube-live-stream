5ea2c96c89c4cef1eea0c3ca5d8cf990467dbf29
Gopu09934 <gopalkumar09934@gmail.com>
2026-06-16 23:12:18 +0530

    Add video download functionality in start.sh
    Create directory for videos and download video from Google Drive.

--- docker/start.sh
+++ docker/start.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+mkdir -p /app/videos
+
+echo "Downloading video..."
+
+gdown "https://drive.google.com/uc?id=${GOOGLE_DRIVE_FILE_ID}" \
+      -O /app/videos/video.mp4
+
 echo "Starting stream..."
 
 ffmpeg \
 -re \
 -stream_loop -1 \
--i "${INPUT_SOURCE}" \
+-i /app/videos/video.mp4 \
 -c:v libx264 \
 -preset veryfast \
--b:v 3500k \
 -c:a aac \
--b:a 128k \
 -f flv \
 "rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
